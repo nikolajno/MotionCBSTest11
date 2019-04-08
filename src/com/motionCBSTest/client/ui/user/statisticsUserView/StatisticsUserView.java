fabaf6552623a462a5996670cd1cb7fda100547e
nikolajno <nikolaj_norborg@hotmail.com>
2019-04-08 14:35:20 +0200

    Hej

--- src/com/motionCBSTest/client/ui/user/statisticsUserView/StatisticsUserView.java
+++ src/com/motionCBSTest/client/ui/user/statisticsUserView/StatisticsUserView.java
@@ -2,14 +2,20 @@ package com.motionCBSTest.client.ui.user.statisticsUserView;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.DataGrid;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.motionCBSTest.shared.User;
 
 public class StatisticsUserView extends Composite {
     interface StatisticsUserViewUiBinder extends UiBinder<HTMLPanel, StatisticsUserView> {
     }
 
     private static StatisticsUserViewUiBinder ourUiBinder = GWT.create(StatisticsUserViewUiBinder.class);
+
+    @UiField
+    DataGrid<User> dataGrid;
 
     public StatisticsUserView() {
         initWidget(ourUiBinder.createAndBindUi(this));
