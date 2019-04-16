9cc3928633e247f0cbaca37a17f49a1d0f2220be
Chri743u <christianaj@live.dk>
2019-04-16 10:57:28 +0200

    GGWP N00B

---
+++ src/com/motionCBSTest/client/ui/admin/MainAdminView/MainAdminView.java
@@ -0,0 +1,76 @@
+package com.motionCBSTest.client.ui.admin.MainAdminView;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.*;
+import com.motionCBSTest.client.ui.admin.DeleteTrainerView.DeleteTrainerView;
+import com.motionCBSTest.client.ui.user.changeUserInfoUserView.ChangeUserInfoUserView;
+import com.motionCBSTest.client.ui.user.statisticsUserView.StatisticsUserView;
+
+// We use DockLayoutPanel which contains option to place panels/widgets in North, east, west, south
+// and center of the screen. We use DeckLayoutPanel in the center.
+
+public class MainAdminView extends Composite {
+
+    private static MainAdminViewUiBinder UiBinder = GWT.create(MainAdminViewUiBinder.class);
+
+    private ChangeUserInfoUserView changeUserInfoUserView;
+    private StatisticsUserView statisticsUserView;
+    private DeleteTrainerView deleteTrainerView;
+
+    @UiField
+    DeckLayoutPanel centerPanel;
+    @UiField
+    Button deleteTrainerBtn;
+    @UiField
+    Button changeBtn;
+    @UiField
+    Button logoutBtn;
+
+    interface MainAdminViewUiBinder extends UiBinder<HTMLPanel, MainAdminView> {}
+
+    // The constructor is creating an instance of the panels and adding them to the DeckLayoutPanel
+    public MainAdminView() {
+        initWidget(UiBinder.createAndBindUi(this));
+
+        changeUserInfoUserView = new ChangeUserInfoUserView();
+        centerPanel.add(changeUserInfoUserView);
+
+        statisticsUserView = new StatisticsUserView();
+        centerPanel.add(statisticsUserView); // Hjælp / forstå
+
+        deleteTrainerView = new DeleteTrainerView();
+        centerPanel.add(deleteTrainerView);
+
+        // The panel that is shown when the application start
+        centerPanel.showWidget(changeUserInfoUserView);
+    }
+
+
+    //This method is used to add the clickHandlers from the logic to the menu buttons
+    public void addClickHandlers(ClickHandler clickHandler) {
+
+        logoutBtn.addClickHandler(clickHandler);
+        deleteTrainerBtn.addClickHandler(clickHandler);
+    }
+
+    // This method is used to change the centerView
+    public void changeView(Widget panel) {
+        centerPanel.showWidget(panel);
+    }
+
+    // The following getters is for view and tables
+    public ChangeUserInfoUserView getChangeUserInfoUserView() {return changeUserInfoUserView; }
+    public StatisticsUserView getStatisticsUserView() {return statisticsUserView; }
+    public DeleteTrainerView getDeleteTrainerView(){return deleteTrainerView;}
+
+    // The following getters is used for the menu buttons
+    public Button getDeleteTrainerBtn(){return deleteTrainerBtn;}
+    public Button getLogoutBtn() {return logoutBtn;}
+    public Button getChangeBtn() {return changeBtn;}
+
+
+
+}
